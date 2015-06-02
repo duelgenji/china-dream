f15c4bbc3f0413d4b85b7d3fb4d96c471bc00e83
王强 <wangqiang02@wondersgroup.com>
2015-06-02 11:32:37 +0800

    initial commit

---
+++ src/main/java/com/dream/entity/Pojo.java
@@ -0,0 +1,13 @@
+package com.dream.entity;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+/**
+ * Created by wangqiang on 15/6/2.
+ */
+public class Pojo extends AbstractPersistable<Long> {
+
+    String content;
+
+
+}
